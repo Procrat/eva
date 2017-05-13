e2ddef9ca277369867cd7b001d77cddb864a4999
Stijn Seghers <stijnseghers@gmail.com>
2017-05-13 19:45:47 +1200

    Implement Insertable for Tasks and test

--- src/db.rs
+++ src/db.rs
@@ -1,13 +1,16 @@
+use super::Task;
+
 use std::io;
 
-use chrono::{DateTime, Duration, UTC, NaiveDateTime};
+use chrono::{DateTime, Duration, NaiveDateTime, UTC};
 use diesel::prelude::*;
-use diesel::types::{FromSql, Integer, HasSqlType, Text};
 use diesel::backend::Backend;
-use diesel::query_source::Queryable;
-use diesel::sqlite::SqliteConnection;
-
-use super::Task;
+use diesel::insertable::ColumnInsertValue;
+use diesel::expression::AsExpression;
+use diesel::expression::helper_types::AsNullableExpr;
+use diesel::query_builder::insert_statement::{InsertStatement, IntoInsertStatement};
+use diesel::sqlite::{Sqlite, SqliteConnection};
+use diesel::types::{FromSql, HasSqlType, Integer, Text};
 
 
 const DATABASE_URL: &'static str = "db.sqlite";
@@ -24,17 +27,25 @@ table! {
 
 embed_migrations!();
 
+
 pub fn make_connection() -> SqliteConnection {
-    let connection = SqliteConnection::establish(DATABASE_URL)
-        .expect(&format!("Error connecting to {}", DATABASE_URL));
-    embedded_migrations::run_with_output(&connection, &mut io::stdout()).unwrap();
-    return connection;
+    make_connection_with(DATABASE_URL)
 }
+
+fn make_connection_with(database_url: &str) -> SqliteConnection {
+    let connection =
+        SqliteConnection::establish(database_url).expect(&format!("Error connecting to {}",
+                                                                  database_url));
+    // TODO run instead of run_with_output + unwrap
+    embedded_migrations::run_with_output(&connection, &mut io::stdout()).unwrap();
+    connection
+}
+
 
 impl<DB> Queryable<(Integer, Text, Integer, Integer, Integer), DB> for Task
     where DB: Backend + HasSqlType<Integer> + HasSqlType<Text>,
           i32: FromSql<Integer, DB>,
-          String: FromSql<Text, DB>,
+          String: FromSql<Text, DB>
 {
     type Row = (i32, String, i32, i32, i32);
 
@@ -43,11 +54,65 @@ impl<DB> Queryable<(Integer, Text, Integer, Integer, Integer), DB> for Task
         let deadline = DateTime::from_utc(naive_deadline, UTC);
         let duration = Duration::seconds(row.3 as i64);
         Task {
-            id: row.0 as u32,
             content: row.1,
             deadline: deadline,
             duration: duration,
             importance: row.4 as u32,
         }
+    }
+}
+
+
+impl<'a> Insertable<tasks::table, Sqlite> for &'a Task {
+    type Values = (ColumnInsertValue<tasks::content, AsNullableExpr<String, tasks::content>>,
+     ColumnInsertValue<tasks::deadline, AsNullableExpr<i32, tasks::deadline>>,
+     ColumnInsertValue<tasks::duration, AsNullableExpr<i32, tasks::duration>>,
+     ColumnInsertValue<tasks::importance, AsNullableExpr<i32, tasks::importance>>);
+
+    fn values(self) -> Self::Values {
+        use diesel::types::IntoNullable;
+
+        (Insertable_column_expr!(tasks::content, self.content.clone(), regular),
+         Insertable_column_expr!(tasks::deadline, self.deadline.timestamp() as i32, regular),
+         Insertable_column_expr!(tasks::duration, self.duration.num_seconds() as i32, regular),
+         Insertable_column_expr!(tasks::importance, self.importance as i32, regular))
+    }
+}
+
+impl<'a, Op> IntoInsertStatement<tasks::table, Op> for &'a Task {
+    type InsertStatement = InsertStatement<tasks::table, Self, Op>;
+
+    fn into_insert_statement(self, target: tasks::table, operator: Op) -> Self::InsertStatement {
+        InsertStatement::no_returning_clause(target, self, operator)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::prelude::*;
+    use diesel;
+
+    #[test]
+    fn test_insert_and_query() {
+        use self::tasks::dsl::tasks;
+
+        let connection = make_connection_with(":memory:");
+
+        let new_task = Task {
+            content: "do me".to_string(),
+            deadline: UTC::now().with_nanosecond(0).unwrap(),
+            duration: Duration::seconds(6),
+            importance: 42,
+        };
+
+        diesel::insert(&new_task)
+            .into(tasks)
+            .execute(&connection)
+            .unwrap();
+
+        let tasks_ = tasks.load::<Task>(&connection).unwrap();
+        assert_eq!(tasks_, [new_task]);
     }
 }

57487904fd47b406b31a8eda8a4e72988fdd3e66
Stijn Seghers <stijnseghers@gmail.com>
2017-08-24 20:17:32 +1200

    Make user configuration possible

--- README.md
+++ README.md
@@ -22,6 +22,16 @@ the Eisenhower scheme, flow, focussing on one task, small-chunking work, eating
 the frog, etc.
 
 
+## Core principles
+
+1. Eva should be as **inobtrusive** as possible to maximise your flow, but
+   **obtrusive** to manage your time.
+2. Eva should maximise your **motivation** while minimising your time
+   **procrastinating**.
+3. **Automatic scheduling** should left to machines since humans are better at
+   deciding importance, deadlines and estimated duration of tasks then at
+   actually scheduling all the things in their lives.
+
 
 ## Installation
 
@@ -144,15 +154,25 @@ Schedule:
     (deadline: Mon 4 Sep 0:00, duration: 48h0, importance: 2)
 ```
 
-## Core principles
 
-1. Eva should be as **inobtrusive** as possible to maximise your flow, but
-   **obtrusive** to manage your time.
-2. Eva should maximise your **motivation** while minimising your time
-   **procrastinating**.
-3. **Automatic scheduling** should left to machines since humans are better at
-   deciding importance, deadlines and estimated duration of tasks then at
-   actually scheduling all the things in their lives.
+## Configuration
+
+Eva Just Works™ without any extra configuration.
+
+There are some things you could change if you really wanted to, by making a file
+called `~/.config/eva/eva.toml` on GNU/Linux, `~/Library/Application
+Support/eva/eva.toml` on Mac OS or
+`C:\Users\<username>\AppData\Roaming\eva\eva.toml` on Windows. You can use '~'
+and refer to environment variables if you want. These are the options you can
+set at the moment, alongside their defaults:
+```toml
+# On GNU/Linux
+database = "~/.local/share/eva/db.sqlite"
+# On Mac OS
+database = "~/Library/Application Support/eva/db.sqlite"
+# On Windows
+database = "C:\Users\<username>\AppData\Roaming\eva\db.sqlite"
+```
 
 
 ## Roadmap
@@ -162,7 +182,7 @@ Schedule:
 - [x] Task persistence
 - [x] Minimal task management interface
 - [x] Automatic scheduling
-- [ ] User configuration
+- [x] User configuration
 - [ ] Manual scheduling of fixed events
 - [ ] Take fixed hours of sleeping and/or working into account
 
---
+++ src/configuration.rs
@@ -0,0 +1,91 @@
+use std::fs;
+use std::path::{Path, PathBuf};
+
+use app_dirs;
+use app_dirs::{AppDataType, AppInfo};
+use config;
+use shellexpand;
+
+const APP_INFO: AppInfo = AppInfo { name: "eva", author: "Stijn Seghers" };
+
+lazy_static! {
+    static ref CONFIG_ROOT: PathBuf = app_dirs::get_app_root(AppDataType::UserConfig, &APP_INFO)
+        .unwrap_or_else(|err| {
+            panic!(format!(
+                    "An error occured while trying to find the configuration directory: {}",
+                    err));
+        });
+    static ref DATA_ROOT: PathBuf = app_dirs::get_app_root(AppDataType::UserData, &APP_INFO)
+        .unwrap_or_else(|err| {
+            panic!(format!(
+                    "An error occured while trying to find the local data directory: {}",
+                    err));
+        });
+}
+
+
+pub fn read() -> config::Config {
+    let config_filename = CONFIG_ROOT.join("eva");
+    let config_filename = config_filename.to_str().unwrap();
+
+    let mut settings = config::Config::new();
+
+    set_defaults(&mut settings)
+        .merge(config::File::with_name(config_filename).required(false))
+        .unwrap_or_else(|err| {
+            panic!(format!("An error occured while reading local configuration file {}.toml: {}",
+                           config_filename,
+                           err));
+        })
+        .merge(config::Environment::with_prefix("eva"))
+        .unwrap_or_else(|err| {
+            panic!(format!("An error occured while reading environment variables: {}", err));
+        });
+
+    expand(&mut settings);
+
+    ensure_paths_exist(&settings);
+
+    settings
+}
+
+
+fn set_defaults(settings: &mut config::Config) -> &mut config::Config {
+    let db_filename = DATA_ROOT.join("db.sqlite");
+    let db_filename = db_filename.to_str().unwrap();
+    settings
+        .set_default("database", db_filename)
+        .unwrap_or_else(|err| {
+            panic!(format!("An error occured while setting configuration defaults: {}", err));
+        })
+}
+
+
+fn expand(settings: &mut config::Config) {
+    let database_filename = settings.get_str("database")
+        .unwrap_or_else(|err| {
+            panic!(format!("An error occured while trying to read database path: {}", err));
+        });
+    let expanded_database_filename = shellexpand::full(&database_filename)
+        .unwrap_or_else(|err| {
+            panic!(format!("An error occured while expanding Eva's configuration: {}", err));
+        });
+    if expanded_database_filename != database_filename {
+        settings.set("database", expanded_database_filename.to_string())
+            .unwrap_or_else(|err| {
+                panic!(format!("An error occured while setting exanded configuration: {}", err));
+            });
+    }
+}
+
+
+fn ensure_paths_exist(settings: &config::Config) {
+    let database_filename = settings.get_str("database")
+        .unwrap_or_else(|err| {
+            panic!(format!("An error occured while trying to read database path: {}", err))
+        });
+    fs::create_dir_all(Path::new(&database_filename).parent().unwrap())
+        .unwrap_or_else(|err| {
+            panic!(format!("An error occured while trying to create database directory: {}", err));
+        })
+}
